ccafcc0346ae21614a91a410f4abe4fc1887b9d7
onyx6838 <giang.mink@gmail.com>
2021-08-03 14:56:30 +0700

    maven sql utils

--- JAVA/Java_Less11_Maven/src/main/java/com/vti/backend/datalayer/AccountRepository.java
+++ JAVA/Java_Less11_Maven/src/main/java/com/vti/backend/datalayer/AccountRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import com.vti.entity.Account;
 import com.vti.utils.JdbcUtils;
+import com.vti.utils.SqlUtils;
 import com.vti.utils.properties.MessageProperties;
 
 /**
@@ -80,7 +81,9 @@ public class AccountRepository implements IAccountRepository {
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
         // set parameter
-        preparedStatement.setInt(1, id);
+        //preparedStatement.setInt(1, id);
+
+        SqlUtils.addParams(preparedStatement, id);
 
         // Step 4: execute query
         ResultSet resultSet = preparedStatement.executeQuery();
@@ -184,9 +187,11 @@ public class AccountRepository implements IAccountRepository {
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
         // set parameter
-        preparedStatement.setString(1, email);
-        preparedStatement.setString(2, username);
-        preparedStatement.setString(3, fullName);
+//        preparedStatement.setString(1, email);
+//        preparedStatement.setString(2, username);
+//        preparedStatement.setString(3, fullName);
+
+        SqlUtils.addParams(preparedStatement, email, username, fullName);
 
         // Step 4: execute query
         int check = preparedStatement.executeUpdate();
